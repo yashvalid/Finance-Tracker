c668fc13f38c6557310981516b6e4174e469e154
yashvalid <patilllyashhh@gmail.com>
2025-05-20 00:16:50 +0530

    few tweaks

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/user.route');
 
 connectDB();
 const corsOptions = {
-    origin: 'http://localhost:5173', // <-- your frontend's URL
+    origin: process.env.ORIGIN, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 200
